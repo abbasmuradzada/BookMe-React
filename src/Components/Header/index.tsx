83c4c5dd426e4b1bc001b086bb76495355ced76d
abbasmuradzada <abbasam@code.edu.az>
2021-02-25 02:38:19 +0400

    react router configured

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -3,44 +3,49 @@ import './style.scss';
 import logo from '../../Assets/Images/bookme.png';
 import callImg from '../../Assets/Images/call.png';
 import Box from '@material-ui/core';
+import { useHistory } from "react-router-dom";
 
 
 const Header:FC = () => {
-  return (
-    <>
-        <header className="container d-flex flex-wrap justify-content-between align-items-center">
-        <div className="header-left d-flex align-items-center">
-            <img src={logo} alt="" />
-            <span className="account">Accountant &nbsp;<i className="fas fa-angle-down"></i></span>
-        </div>
-        <div className="header-right d-flex flex-column align-items-end justify-content-between">
-            <div className="header-contact d-flex align-items-center">
-                <i className="fab fa-facebook-f"></i>
-                <i className="fab fa-twitter"></i>
-                <i className="fab fa-google-plus-g"></i>
-                <p className="quotation-text">ABSOLUTELY Free <br />
-                    QUOTATION NOW
-                </p>
-                <div className="partition"></div>
-                <p className="call-number d-flex align-items-center">
-                    <img src={callImg} alt="" />
-                    215 123 4567
-                </p>
+    const history = useHistory();
+    const goToBlog = () => {
+        history.push('/blog')
+    }
+    return (
+        <>
+            <header className="container d-flex flex-wrap justify-content-between align-items-center">
+            <div className="header-left d-flex align-items-center">
+                <img src={logo} alt="" />
+                <span className="account">Accountant &nbsp;<i className="fas fa-angle-down"></i></span>
             </div>
-            <nav>
-                <ul className="d-flex">
-                    <li className="active nav-home"><a>home</a></li>
-                    <li className="nav-about"><a>about me</a></li>
-                    <li className="nav-services"><a>services</a></li>
-                    <li className="nav-whyme"><a>why me</a></li>
-                    <li className="nav-features"><a>features</a></li>
-                    <li className="nav-home"><a href="./blog.html">blog</a></li>
-                    <li className="nav-contact"><a>contact</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-    </>
+            <div className="header-right d-flex flex-column align-items-end justify-content-between">
+                <div className="header-contact d-flex align-items-center">
+                    <i className="fab fa-facebook-f"></i>
+                    <i className="fab fa-twitter"></i>
+                    <i className="fab fa-google-plus-g"></i>
+                    <p className="quotation-text">ABSOLUTELY Free <br />
+                        QUOTATION NOW
+                    </p>
+                    <div className="partition"></div>
+                    <p className="call-number d-flex align-items-center">
+                        <img src={callImg} alt="" />
+                        215 123 4567
+                    </p>
+                </div>
+                <nav>
+                    <ul className="d-flex">
+                        <li className="active nav-home"><a>home</a></li>
+                        <li className="nav-about"><a>about me</a></li>
+                        <li className="nav-services"><a>services</a></li>
+                        <li className="nav-whyme"><a>why me</a></li>
+                        <li className="nav-features"><a>features</a></li>
+                        <li onClick={goToBlog} className="nav-home"><a href="">blog</a></li>
+                        <li className="nav-contact"><a>contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        </>
   );
 }
 
--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -1,13 +1,28 @@
 import React, {FC} from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
+import { BrowserRouter as Router, Switch, Route, Redirect, useHistory  } from "react-router-dom";
+
 
 const Layout:FC = () => {
   return (
     <div className="">
-        <Header/>
-        RenderBody
+        <Router>
+            <Header/>
+        
+            <Switch>
+                <Route exact path="/">
+                    <Redirect to="/home"/>
+                </Route>  
+                <Route path = "/home">
+                    <div className='container'>Home Page</div>
+                </Route>
+                <Route path = "/blog">
+                    <div className='container'>Blog Page</div>
+                </Route>
+            </Switch>
         <Footer/>
+    </Router>
     </div>
   );
 }
